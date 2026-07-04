cec6a6b705733af853ce6cbd3ec5a36f2a1b3246
BrunoPer3s <brunoperes7@hotmail.com>
2026-07-04 17:08:44 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/br/ufes/deliverypedidos/DeliverypedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.deliverypedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverypedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
